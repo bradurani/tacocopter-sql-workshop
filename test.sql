cd4ebd7db8e5520a476da0975aabd220580a2195
Brad Urani <bradurani@gmail.com>
2015-10-22 17:08:33 +0000

    Delete test.sql

--- test.sql
+++
@@ -1,6 +0,0 @@
-SELECT stores.*, COUNT(*) 
-FROM stores 
-JOIN store_tacos ON stores.id = store_tacos.store_id 
-JOIN tacos ON store_tacos.taco_id = tacos.id
-           AND tacos.vegetarian = true
-GROUP by stores.id;
